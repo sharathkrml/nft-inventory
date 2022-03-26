fa8b3e4738dd827d5eb1d0bc1206404313c9cd9f
sharath V <sharathkrml@gmail.com>
2022-03-27 00:23:44 +0530

    testing alchemy nft api

---
+++ constants.js
@@ -0,0 +1,2 @@
+export const MUMBAI_URL = "https://polygon-mumbai.g.alchemy.com/v2/";
+export const POLYGON_URL = "https://polygon-mainnet.g.alchemy.com/v2/";
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,28 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { useEffect } from "react";
+import { createAlchemyWeb3 } from "@alch/alchemy-web3";
+import { MUMBAI_URL, POLYGON_URL } from "../constants";
 export default function Home() {
+  const mumbaiweb3 = createAlchemyWeb3(
+    `${MUMBAI_URL}${process.env.NEXT_PUBLIC_API}`
+  );
+  const polygonweb3 = createAlchemyWeb3(
+    `${POLYGON_URL}${process.env.NEXT_PUBLIC_API}`
+  );
+  const ownerAddr = "0x7fdc80eEE466aAA3339c01BEb8dDADBc98ECEbf2";
+  useEffect(async () => {
+    const mumbainfts = await mumbaiweb3.alchemy.getNfts({
+      owner: ownerAddr,
+    });
+    console.log(mumbainfts);
+    const ploygonnfts = await polygonweb3.alchemy.getNfts({
+      owner: ownerAddr,
+    });
+    console.log(ploygonnfts);
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -17,7 +37,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -58,12 +78,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
